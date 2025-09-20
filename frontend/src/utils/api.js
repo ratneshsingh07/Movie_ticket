51a8289641614aedb0bca75a29bd0757dd7a1f79
ratneshsingh07 <ratneshsingh@snapecabs.com>
2025-09-20 22:52:45 +0530

    api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // API Base URL
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = https://movie-ticket-rxxa.onrender.com/api;
 
 // Create axios instance
 const api = axios.create({
